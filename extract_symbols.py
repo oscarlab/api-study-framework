41c881eef443f50c6ca4984ad03d0862526d1a0f
nafees ahmed abdul <nafees4all@janice.(none)>
2014-06-26 16:33:58 -0400

    modified extract_symbols.py

--- extract_symbols.py
+++ extract_symbols.py
@@ -3,17 +3,18 @@ import subprocess
 import sys
 import re
 binaryname = sys.argv[1]
-process1 = subprocess.Popen(["readelf", "-a", binaryname],
+process = subprocess.Popen(["readelf", "--dyn-syms", binaryname],
                              stdout=subprocess.PIPE
                            )
 count = 0
-str = "example write rread readif read@pit"
-str1 =  process1.communicate()[0] 
-#print str1
-with open("listfile.txt") as f:
-    for line in f:
-	match = re.search("\\b"+line.strip()+"\\b",str1)
-	if match:
-	   print match.group()
-           count = count + 1
-print "Total number of syscalls made out of 1523:" ,count
+#str =  process.communicate()[0] 
+for line in process.stdout:
+	parts = line.split()
+#	print len(parts)
+	if len(parts) > 7:
+		if parts[3] == 'FUNC' and (parts[4] == 'GLOBAL' or parts[4] == 'WEAK'):
+			count = count + 1
+ 			f = parts[7].split("@")[0]
+			print f
+print "Total number of functions in " + binaryname + " are " , count
+
