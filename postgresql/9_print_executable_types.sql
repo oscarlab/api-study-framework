425e6a1f1b2a3cac6d49726749adeac951cf7b2b
Chia-Che Tsai <chitsai@cs.stonybrook.edu>
2014-10-07 10:52:52 -0400

    output results

---
+++ postgresql/9_print_executable_types.sql
@@ -0,0 +1,75 @@
+CREATE TYPE output AS (executable_type VARCHAR, popularity INT);
+
+CREATE OR REPLACE FUNCTION query_output()
+RETURNS SETOF output AS $$
+DECLARE
+	r output%ROWTYPE;
+
+BEGIN
+	FOR r IN (
+		SELECT concat('ELF Binary'), count(*)
+		FROM binary_list WHERE type = 'exe'
+	) LOOP
+		RETURN NEXT r;
+	END LOOP;
+
+	FOR r IN (
+		SELECT concat('Scripts'), count(*)
+		FROM binary_list WHERE type = 'scr'
+	) LOOP
+		RETURN NEXT r;
+	END LOOP;
+
+	CREATE TEMP TABLE exetype (executable_type VARCHAR, popularity INT);
+
+	INSERT INTO exetype
+		SELECT t2.binary_name, count(*)
+		FROM binary_interp AS t1 INNER JOIN binary_id AS t2
+		ON t1.interp = t2.id
+		GROUP BY t2.binary_name;
+
+	For r IN (
+		SELECT concat('Python') AS executable_type, sum(popularity)
+		FROM exetype WHERE executable_type LIKE '%python%'
+	) LOOP
+		RETURN NEXT r;
+	END LOOP;
+
+	For r IN (
+		SELECT concat('Perl') AS executable_type, sum(popularity)
+		FROM exetype WHERE executable_type LIKE '%perl%'
+	) LOOP
+		RETURN NEXT r;
+	END LOOP;
+
+	For r IN (
+		SELECT concat('Ruby') AS executable_type, sum(popularity)
+		FROM exetype WHERE executable_type LIKE '%ruby%'
+	) LOOP
+		RETURN NEXT r;
+	END LOOP;
+
+	For r IN (
+		SELECT concat('Dash') AS executable_type, sum(popularity)
+		FROM exetype WHERE executable_type = '/bin/sh' OR executable_type = '/bin/dash'
+	) LOOP
+		RETURN NEXT r;
+	END LOOP;
+
+	FOR r IN (
+		SELECT concat('Bash') AS executable_type, sum(popularity)
+		FROM exetype WHERE executable_type = '/bin/bash'
+	) LOOP
+		RETURN NEXT r;
+	END LOOP;
+
+	DROP TABLE exetype;
+
+	RETURN;
+END
+$$ LANGUAGE plpgsql;
+
+\copy (SELECT * FROM query_output()) TO '/tmp/executable-types.csv' WITH CSV
+
+DROP FUNCTION query_output();
+DROP TYPE output CASCADE;
---
+++ postgresql/9_print_library_popularity.sql
@@ -0,0 +1,26 @@
+CREATE TYPE output AS (symbol VARCHAR, popularity FLOAT);
+
+CREATE FUNCTION query_output(libpkg VARCHAR, libname VARCHAR)
+RETURNS SETOF output AS $$
+DECLARE
+	p INT := (SELECT id FROM package_id WHERE package_name = libpkg);
+	b INT := (SELECT id FROM binary_id  WHERE binary_name  = libname);
+
+BEGIN
+
+	RETURN QUERY
+	SELECT DISTINCT t2.symbol_name, t1.popularity
+	FROM call_popularity AS t1
+	INNER JOIN
+	binary_symbol AS t2
+	ON  t1.pkg_id = p AND t2.bin_id = b
+	AND t1.pkg_id = p AND t1.bin_id = b
+	ORDER BY popularity DESC, symbol_name;
+
+END
+$$ LANGUAGE plpgsql;
+
+\copy (SELECT * FROM query_output('libc6', '/lib/x86_64-linux-gnu/libc-2.15.so')) TO '/tmp/libc-popularity.csv' WITH CSV
+
+DROP FUNCTION query_output(libpkg VARCHAR, libname VARCHAR);
+DROP TYPE output;
