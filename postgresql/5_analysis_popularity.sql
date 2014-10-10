65ce5981d19b2ce1f383f2ba115afa9168440b3f
Chia-Che Tsai <chiache@miss-piggy.oscar.cs.stonybrook.edu>
2014-10-10 16:36:21 -0400

    fixes

--- postgresql/5_analysis_popularity.sql
+++ postgresql/5_analysis_popularity.sql
@@ -48,7 +48,11 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION get_pop(pop FLOAT)
 RETURNS FLOAT AS $$
 BEGIN
-	RETURN 1.0 - 10.0 ^ (-pop);
+	IF pop > 6 THEN
+		RETURN 1.0;
+	ELSE
+		RETURN 1.0 - 10.0 ^ (-pop);
+	END IF;
 END
 $$ LANGUAGE plpgsql;
 
