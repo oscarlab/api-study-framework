fca79a0a555e976c7ea6b373a3fb924eab30ba64
Chia-Che Tsai <chitsai@cs.stonybrook.edu>
2016-05-10 10:41:56 -0400

    minor

--- postgresql/2_analysis/instr.sql
+++ postgresql/2_analysis/instr.sql
@@ -26,12 +26,37 @@ BEGIN
 
 	time1 := clock_timestamp();
 
+	CREATE TEMP TABLE IF NOT EXISTS pkg_bin (
+		bin_id INT NOT NULL,
+		callgraph BOOLEAN,
+		PRIMARY KEY(bin_id));
+	INSERT INTO pkg_bin
+		SELECT DISTINCT bin_id, callgraph FROM binary_list
+		WHERE pkg_id = p AND (type = 'exe' OR type = 'lib');
+
+	IF NOT EXISTS (
+		SELECT * FROM pkg_bin
+	) THEN
+		UPDATE package_id SET footprint = True WHERE id = p;
+		RETURN;
+	END IF;
+
+	IF EXISTS (
+		SELECT * FROM pkg_bin WHERE callgraph = False
+	) THEN
+		RAISE EXCEPTION 'binary not resolved: %', p;
+	END IF;
+
+
 	DELETE FROM package_instr_count WHERE pkg_id = p;
 
 	INSERT INTO package_instr_count
-		SELECT p, instr, SUM(count) FROM binary_instr_usage
-		WHERE pkg_id = p
-		GROUP BY instr;
+		SELECT p, t1.instr, SUM(t1.count) FROM
+		binary_instr_usage AS t1
+		INNER JOIN
+		pkg_bin AS t2
+		ON t1.pkg_id = p AND t1.bin_id = t2.bin_id
+		GROUP BY t1.instr;
 
 	time2 := clock_timestamp();
 	RAISE NOTICE 'Time: %', time2 - time1;
