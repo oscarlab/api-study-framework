b38f7e002b80a37ceab2ebc9e88d908f7c7bed75
Amogh Akshintala <aakshintala@cs.stonybrook.edu>
2017-02-27 23:24:36 +0000

    Don't bother adding call targets to the entries. Entries are only determined from ELF header. GLOBAL Symbols only. May need to come back and figure out what to do with WEAK functions later.

--- objdump_linear.py
+++ objdump_linear.py
@@ -724,9 +724,10 @@ def get_callgraph(binary_name, sql=None, pkg_id=None, bin_id=None):
 											rel_entries[target_addr],
 											size, binbytes))
 							else:
-								val2p = val2ptr(target_addr, ptr_size)
-								if val2p < self.start or val2p >= self.end:
-									self.add_entry(val2ptr(target_addr, ptr_size))
+								#val2p = val2ptr(target_addr, ptr_size)
+								#if val2p < self.start or val2p >= self.end:
+								#	print "Adding to entries:", hex(val2p), hex(self.start), hex(self.end)
+								#	self.add_entry(val2ptr(target_addr, ptr_size))
 								self.cur_func.instrs.append(InstrCall(address,
 											disassembly,
 											target_addr, size, binbytes))
@@ -739,9 +740,10 @@ def get_callgraph(binary_name, sql=None, pkg_id=None, bin_id=None):
 							logging.info(target_addr)
 							logging.info(" ")
 					elif target:
-						val2p = val2ptr(target,ptr_size)
-						if val2p < self.start or val2p >= self.end:
-							self.add_entry(val2p)
+#						val2p = val2ptr(target,ptr_size)
+#						if val2p < self.start or val2p >= self.end:
+#							print "Adding to entries:", hex(val2p), hex(self.start), hex(self.end)
+#							self.add_entry(val2p)
 						self.cur_func.instrs.append(InstrCall(address,
 											disassembly,
 											target, size, binbytes))
@@ -846,16 +848,13 @@ def get_callgraph(binary_name, sql=None, pkg_id=None, bin_id=None):
 
 				if next not in dynsym_list.keys():
 					self.cur_func = Func(next)
-					#print hex(next), hex(self.start), hex(self.end)
 				else:
 					size = dynsym_list[next]
 					size = int(size)
 					if size == 0:
 						self.cur_func = Func(next)
-					#	print hex(next), hex(self.start), hex(self.end)
 					else:
 						self.cur_func = Func(next, next+size)
-					#	print hex(next), hex(self.start), hex(next+size)
 				self.entries.remove(next)
 				self.processed_entries.append(next)
 
@@ -884,8 +883,9 @@ def get_callgraph(binary_name, sql=None, pkg_id=None, bin_id=None):
 			continue
 		symbol_name = match.group(1)
 		size = parts[2]
-		if is_hex(size):
-			size = int(size, 16)
+		if ishex(size):
+			logging.info('Why is the size hex? Why is nothing standardized?')
+			size = hex2int(size)
 		dynsym_list[addr] = size
 
 	if process.wait() != 0:
