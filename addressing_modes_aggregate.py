e9fe5da048b29ce27ec351132070647504d6c1d8
Amogh Akshintala <aakshintala@cs.stonybrook.edu>
2018-03-28 14:56:08 +0000

    bugfixes

--- addressing_modes_aggregate.py
+++ addressing_modes_aggregate.py
@@ -7,13 +7,17 @@ out_dirname = "/filer/aggregateam/"
 for dir2name in os.listdir(in_dirname):
 	packageDir = os.path.join(out_dirname,dir2name)
 	if os.path.exists(packageDir):
-		shutil.rmtree(packageDir)
+		continue
+		#shutil.rmtree(packageDir)
 	os.mkdir(packageDir)
 	with open(os.path.join(packageDir, 'aggregate.csv'), 'w+') as csvFile:
 		AMs = {}
 		for fname in os.listdir(os.path.join(in_dirname, dir2name)):
 			for line in open(os.path.join(in_dirname, dir2name, fname)):
-				key, value = line.strip().split(': ')
+				try:
+					key, value = line.strip().split(': ')
+				except ValueError:
+					continue
 				value = int(value)
 				if key in AMs.keys():
 					AMs[key] += value
